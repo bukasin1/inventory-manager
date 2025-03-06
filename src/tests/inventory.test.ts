4dfd1625672185cac296ee53c9b43633b89d36fe
bukasin1 <bukasin1@gmail.com>
2025-03-06 10:15:18 +0100

    feat: update general tests

--- src/tests/inventory.test.ts
+++ src/tests/inventory.test.ts
@@ -2,27 +2,48 @@ import request from "supertest";
 import app from "../app";
 
 describe("Inventory API", () => {
-  it("should add an item", async () => {
-    const res = await request(app)
-      .post("/inventory/apple/add")
-      .send({ quantity: 10, expiry: Date.now() + 10000 });
-    expect(res.status).toBe(201);
-  });
+  describe("General tests", () => {
+    const t0 = Date.now();
+    it("should add an item", async () => {
+      const res = await request(app)
+        .post("/inventory/apple/add")
+        .send({ quantity: 10, expiry: t0 + 10000 });
+      expect(res.status).toBe(201);
+    });
 
-  it("should get item quantity", async () => {
-    const res = await request(app).get("/inventory/apple/quantity");
-    expect(res.status).toBe(200);
-  });
+    it("should get item quantity", async () => {
+      const res = await request(app).get("/inventory/apple/quantity");
+      expect(res.status).toBe(200);
+      expect(res.body.quantity).toBe(10);
+      expect(res.body.validTill).toBe(t0 + 10000);
+    });
 
-  it("should sell an item", async () => {
-    const res = await request(app)
-      .post("/inventory/apple/sell")
-      .send({ quantity: 5 });
-    expect(res.status).toBe(200);
-  });
+    it("should sell an item", async () => {
+      const res = await request(app)
+        .post("/inventory/apple/sell")
+        .send({ quantity: 5 });
+      expect(res.status).toBe(200);
+    });
 
-  it("should get item quantity", async () => {
-    const res = await request(app).get("/inventory/apple/quantity");
-    expect(res.status).toBe(200);
+    it("should get item quantity", async () => {
+      const res = await request(app).get("/inventory/apple/quantity");
+      expect(res.status).toBe(200);
+      expect(res.body.quantity).toBe(5);
+      expect(res.body.validTill).toBe(t0 + 10000);
+    });
+
+    it("should sell an item", async () => {
+      const res = await request(app)
+        .post("/inventory/apple/sell")
+        .send({ quantity: 5 });
+      expect(res.status).toBe(200);
+    });
+
+    it("should get item quantity", async () => {
+      const res = await request(app).get("/inventory/apple/quantity");
+      expect(res.status).toBe(200);
+      expect(res.body.quantity).toBe(0);
+      expect(res.body.validTill).toBe(null);
+    });
   });
 });
