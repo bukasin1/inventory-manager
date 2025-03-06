564f4608518a11973522a7768f96ca53c5024177
bukasin1 <bukasin1@gmail.com>
2025-03-06 12:03:20 +0100

    chore: move isTestingLocalhost to test utils

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,20 +1,9 @@
 import dotenv from "dotenv";
+import { isTestingLocalhost } from "./utils";
 dotenv.config();
 
-export function isTestingLocalhost() {
-  const url = new URL(process.env.DATABASE_URL || '');
-  if (
-    !url.hostname.includes('localhost') &&
-    !url.hostname.includes('127.0.0.1') &&
-    !url.hostname.includes('postgresql')
-  ) {
-    console.log('Tests must be run on local db');
-    process.exit(1);
-  }
-}
-
 const setup = async () => {
-  isTestingLocalhost()
+  isTestingLocalhost();
 };
 
 export default setup;
--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,3 +1,15 @@
+export function isTestingLocalhost() {
+  const url = new URL(process.env.DATABASE_URL || "");
+  if (
+    !url.hostname.includes("localhost") &&
+    !url.hostname.includes("127.0.0.1") &&
+    !url.hostname.includes("postgresql")
+  ) {
+    console.log("Tests must be run on local db");
+    process.exit(1);
+  }
+}
+
 export const waitUntil = async (targetTime: number) => {
   const now = Date.now();
   const remainingTime = targetTime - now;
